ee243915f8de149c1d82f6b213ae9d7ba047944d
zhigang90 <jsnzg@163.com>
2017-06-09 10:54:07 -0500

    print fxsx

--- cimmp2force.f
+++ cimmp2force.f
@@ -796,6 +796,7 @@ C      call matcopy('DDT','XF')
       ixadr=mataddr('XF')
 
       call matprint('XF',6)
+      call matprint('fxsx',6)
 C
 C  add -<X|Fx> to forces:
 C  NOTE only one-electron part left of Fx
